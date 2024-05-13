0d34b160c19a47c8adf9351cb32e1c39b7e3ec22
Randika Piyathissa <89309192+Randika00@users.noreply.github.com>
2024-05-13 19:24:24 +0530

    initial commit for desk

---
+++ Student Help Desk/Script/Sign in.js
@@ -0,0 +1,25 @@
+
+function pwdConFunction(){
+			var pwd = document.getElementById("pwd");
+			
+			if (pwd.type === "password")
+			{
+				pwd.type = "text";
+			}
+			else
+			{
+				pwd.type = "password";
+			}	
+			
+			
+			var pwdCon = document.getElementById("conPwd");
+			
+			if (pwdCon.type === "password")
+			{
+				pwdCon.type = "text";
+			}
+			else
+			{
+				pwdCon.type = "password";
+			}
+		}
